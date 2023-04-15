da20852e6ff5ecbad3dcf8c87c82ac3b94094d0e
Marius <marius.ihring@icloud.com>
2023-04-15 21:10:36 +0200

    refactoring

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,3 @@
-use actix_web::{dev::Server, web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use std::net::TcpListener;
-
-async fn health_check(req: HttpRequest) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
-        .listen(listener)?
-        .run();
-
-    Ok(server)
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
