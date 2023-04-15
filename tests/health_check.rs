b279756a1a58c44bad536a130ca85873f98e76c6
Marius <marius.ihring@icloud.com>
2023-04-15 23:40:27 +0200

    working connection to db and configuration loading

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,4 +1,7 @@
 use std::net::TcpListener;
+
+use sqlx::{Connection, PgConnection};
+use zero2prod::configuration::get_configuration;
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
     let port = listener.local_addr().unwrap().port();
@@ -26,6 +29,12 @@ async fn health_check_works() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app();
+    let configuration = get_configuration().expect("failed to read config file");
+    let connection_string = configuration.database.connection_string();
+    let connection = PgConnection::connect(&connection_string)
+        .await
+        .expect("failed to connect to postgres");
+
     let client = reqwest::Client::new();
     let body = "name=le&20guin&email=ursula_le_guin%40gmail.com";
     let response = client
