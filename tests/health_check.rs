5fc37346fec2b24d68014f5f570bd8e3791c1744
Marius <marius.ihring@icloud.com>
2023-04-16 13:18:57 +0200

    moved to pool made first query

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -5,6 +5,7 @@ use zero2prod::configuration::get_configuration;
 fn spawn_app() -> String {
     let listener = TcpListener::bind("127.0.0.1:0").expect("failed to bind random port");
     let port = listener.local_addr().unwrap().port();
+    let configuration = get_configuration().expect("failed to read config");
     let server = zero2prod::startup::run(listener).expect("Failed to bind address");
     let _ = tokio::spawn(server);
 
