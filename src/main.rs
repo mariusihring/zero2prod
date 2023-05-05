a407697e5fa143bff4fc15fd5e9d1aba2ea49166
Marius <marius.ihring@icloud.com>
2023-05-05 23:39:23 +0200

    optimizing the docker container

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use secrecy::ExposeSecret;
 use sqlx::PgPool;
 use std::net::TcpListener;
+use sqlx::postgres::PgPoolOptions;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -12,8 +13,8 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
     //configuring tha rustl
     let configuration = get_configuration().expect("failed to get config");
-    let connection_pool =
-        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
+    let connection_pool = PgPoolOptions::new().acquire_timeout(std::time::Duration::from_secs(2)).
+        connect_lazy(configuration.database.connection_string().expose_secret())
             .expect("failed to connect to postgres");
     let address = format!("{}:{}", configuration.application.host, configuration.application.port);
     let listener = TcpListener::bind(address)?;
