02d69280b20ae04de338bbdf1a910118f24771dc
Marius <marius.ihring@icloud.com>
2023-05-27 17:30:02 +0200

    end of chapter 6.5.0

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -25,6 +25,7 @@ pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
 }
+
 async fn spawn_app() -> TestApp {
     Lazy::force(&TRACING);
 
@@ -82,6 +83,35 @@ async fn health_check_works() {
 
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_200_when_fields_are_present_but_empty() {
+    //Arrange
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=Ursula&email=", "empty email"),
+        ("name=Ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+
+    for (body, description) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("Failed to execute request");
+
+        assert_eq!(
+            200,
+            response.status().as_u16(),
+            "The Api did not return a 200 OK when the payload was {}",
+            description
+        )
+    }
 }
 
 #[tokio::test]
