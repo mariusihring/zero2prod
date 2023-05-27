3a721e05cb89ec5a871a859c36cbf9590429e663
Marius <marius.ihring@icloud.com>
2023-05-27 23:14:48 +0200

    End chapter 6.13.1

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -85,34 +85,32 @@ async fn health_check_works() {
     assert_eq!(Some(0), response.content_length());
 }
 
-//#[tokio::test]
-//async fn subscribe_returns_a_200_when_fields_are_present_but_invalid() {
-//arrange
-//   let app = spawn_app().await;
-// let client = reqwest::client::new();
-//   let test_cases = vec![
-//      ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
-//       ("name=ursula&email=", "empty email"),
-//       ("name=ursula&email=definitely-not-an-email", "invalid email"),
-//   ];
-//
-//   for (body, description) in test_cases {
-//      let response = client
-//        .post(&format!("{}/subscriptions", &app.address))
-//    .header("content-type", "application/x-www-form-urlencoded")
-//      .body(body)
-//       .send()
-//       .await
-//          .expect("failed to execute request");
-//
-//      assert_eq!(
-//        400,
-//      response.status().as_u16(),
-//    "the api did not return a 200 ok when the payload was {}",
-//          description
-//     )
-// }
-//}
+#[tokio::test]
+async fn subscribe_returns_a_200_when_fields_are_present_but_invalid() {
+    let app = spawn_app().await;
+    let client = reqwest::Client::new();
+    let test_cases = vec![
+        ("name=&email=ursula_le_guin%40gmail.com", "empty name"),
+        ("name=ursula&email=", "empty email"),
+        ("name=ursula&email=definitely-not-an-email", "invalid email"),
+    ];
+    for (body, description) in test_cases {
+        let response = client
+            .post(&format!("{}/subscriptions", &app.address))
+            .header("content-type", "application/x-www-form-urlencoded")
+            .body(body)
+            .send()
+            .await
+            .expect("failed to execute request");
+
+        assert_eq!(
+            400,
+            response.status().as_u16(),
+            "the api did not return a 200 ok when the payload was {}",
+            description
+        )
+    }
+}
 
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
